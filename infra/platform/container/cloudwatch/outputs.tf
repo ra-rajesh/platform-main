25c1ccf7b8f580fb2ef4aea39d18aa744590bce9
Rajesh R <your-email@example.com>
2025-10-01 16:11:33 +0530

    fixing ecr duplication error2

--- infra/platform/container/cloudwatch/outputs.tf
+++ infra/platform/container/cloudwatch/outputs.tf
@@ -1,2 +1,8 @@
-output "log_group" { value = aws_cloudwatch_log_group.docker.name }
-output "ssm_param" { value = aws_ssm_parameter.agent_config.name }
+output "docker_log_group_name" {
+  value = aws_cloudwatch_log_group.docker.name
+}
+
+# Returns the created SSM parameter name, or "" when ssm_param_name = ""
+output "ssm_param" {
+  value = try(aws_ssm_parameter.agent_config[0].name, "")
+}
