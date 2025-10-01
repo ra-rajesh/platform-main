a5aa5928637cde7cc994f8d72d12cc17c609ae2f
Rajesh R <your-email@example.com>
2025-10-01 15:38:18 +0530

    fixing ecr duplication error

--- infra/environments/stage/stacks/ecr/main.tf
+++ infra/environments/stage/stacks/ecr/main.tf
@@ -12,7 +12,3 @@ module "ecr" {
   # Be explicit: never publish to SSM
   ssm_prefix = ""
 }
-
-output "repository_names" { value = module.ecr.repository_names }
-output "repository_arns" { value = module.ecr.repository_arns }
-output "repository_urls" { value = module.ecr.repository_urls }
