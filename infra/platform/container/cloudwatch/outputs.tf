c5dbbdc491ba69f6df40067d3a318ee34994efe2
Rajesh R <your-email@example.com>
2025-10-01 16:32:49 +0530

    fixing cw duplication error1

--- infra/platform/container/cloudwatch/outputs.tf
+++ infra/platform/container/cloudwatch/outputs.tf
@@ -2,7 +2,17 @@ output "docker_log_group_name" {
   value = aws_cloudwatch_log_group.docker.name
 }
 
-# Returns the created SSM parameter name, or "" when ssm_param_name = ""
+# Name of the actually-created SSM parameter ("" when disabled)
 output "ssm_param" {
   value = try(aws_ssm_parameter.agent_config[0].name, "")
+}
+
+# Pass-through of the requested SSM param name ("" when disabled)
+output "ssm_param_name" {
+  value = var.ssm_param_name
+}
+
+# Back-compat alias, in case any stacks refer to module.cloudwatch_app.log_group
+output "log_group" {
+  value = aws_cloudwatch_log_group.docker.name
 }
