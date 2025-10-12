21830ea5d22de7b628ed2832c9e01e77485ac556
Rajesh R <your-email@example.com>
2025-10-12 19:05:27 +0530

    adding env in ec2 role name2

--- infra/platform/modules/iam/ssm_instance/main.tf
+++ infra/platform/modules/iam/ssm_instance/main.tf
@@ -113,11 +113,3 @@ resource "aws_iam_role_policy_attachment" "s3_readonly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
 
-# ---------- Outputs ----------
-output "iam_role_name" {
-  value = local.role_name_effective
-}
-
-output "instance_profile_name" {
-  value = local.profile_name_effective
-}
